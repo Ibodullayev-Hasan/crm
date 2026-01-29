e4d75c2e0ace3532f2c8c2671660ba803a6e913e
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2026-01-29 23:01:37 +0500

    logger va  port chaqirildi va sozlandi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
+import { createWinstonLogger } from './common/services';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: createWinstonLogger()
+  });
+
+  const logger = new Logger('CRM_api');
+
+  try {
+    const configService = app.get(ConfigService);
+
+    const port = configService.get<number>("port") || 3003;
+
+    await app.listen(port, () => {
+      logger.log(`Server run on port:${port}`)
+    });
+
+  } catch (error: any) {
+    logger.error(error)
+  }
 }
 bootstrap();
