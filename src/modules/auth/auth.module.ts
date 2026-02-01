be7c1fc03be72169c4aacdde420d364e46bbf581
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2026-02-01 17:14:53 +0500

    tokengenerator va localstrategylar royxatdan otkazildi

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { TokenGenerator } from 'src/common/services';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies';
+import { JwtConfig } from 'src/config';
 
 @Module({
+  imports: [
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.register(JwtConfig),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    TokenGenerator,
+    LocalStrategy
+  ],
+  exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
