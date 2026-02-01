fa6d7902ba368c4a70364fdf6e8aba949eb561be
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2026-02-01 15:41:14 +0500

    swagger, cors, globalPipe va helmet  configuratsiya qilindi

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { createWinstonLogger } from './common/services';
+import { CorsConfig, setupGlobalPipes, SwaggerConfig } from './config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,8 +16,16 @@ async function bootstrap() {
 
   try {
     const configService = app.get(ConfigService);
-    
-    const port = configService.get<number>("port") || 3003;
+
+    app.setGlobalPrefix(configService.get<string>(`server.prefix`));
+    app.enableVersioning({ type: VersioningType.URI, defaultVersion: configService.get<string>(`server.version`) });
+
+    SwaggerConfig(app);
+    setupGlobalPipes(app);
+    CorsConfig(app);
+    app.use(helmet());
+
+    const port = configService.get<number>("server.port") || 3003;
 
     await app.listen(port, () => {
       logger.log(`Server run on port:${port}`)
