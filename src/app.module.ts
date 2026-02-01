6eefd14be2aec4ce5d35b8bc6e80259d7bc2f01c
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2026-02-01 17:10:57 +0500

    jwtmodule royxatdan otkazildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import configuration from './config/configuration';
-import dbConfig from './db/config/db.config';
-
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { APP_FILTER } from '@nestjs/core';
 import { AllexceptionFilter } from './common/filters';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfig } from './config';
+
+import configuration from './config/configuration';
+import dbConfig from './db/config/db.config';
 
 @Module({
   imports: [
@@ -18,6 +19,8 @@ import { AllexceptionFilter } from './common/filters';
       envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`,
       load: [configuration, dbConfig],
     }),
+
+    JwtModule.registerAsync(JwtConfig),
 
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
